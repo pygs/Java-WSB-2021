14c750f1bb8864e6b91c3d08970efe5dd60dfbb4
pygs <pygs1500@gmail.com>
2022-02-03 21:23:53 +0100

    Zadanie 13

--- Main.java
+++ Main.java
@@ -8,9 +8,6 @@ public class Main {
     public static void main(String[] args) {
         Pet dog = new Pet("canis");
         dog.name = "Szarik";
-
-        System.out.println("nazwa psa: " + dog.name);
-
         Pet cat = new Pet("felis");
         cat.name = "Sierściuch";
         Phone Oneplus = new Phone("6", "Oneplus", 2017, 2300.0, 6.6, "Android 11");
@@ -18,7 +15,7 @@ public class Main {
         Human me = new Human("Jakub", "Wasiniewski", 21, 5000000.0);
         me.pet = cat;
         me.mobile = Oneplus;
-        
+
 
         Human brother = new Human("Michał", "Wasiniewski", 33, 3500.0, 1);
 
@@ -29,31 +26,26 @@ public class Main {
         LPG auto2 = new LPG("Rav4", "Toyota", 2019, 100000.0, "SUV", 9000.0, Boolean.FALSE);
         Electric Cybertruck = new Electric("Cybertruck", "Tesla", 2023, 160000.0, "SUV", 0.0, Boolean.TRUE);
 
-        dog.feed();
-        dog.feed(2.0);
-
-        auto1.refuel();
-        Cybertruck.refuel();
-        me.getCar(1);
-
-        me.setCar(auto1, 0);
-        me.setCar(auto2, 1);
-        me.setCar(Cybertruck, 2);
-        me.getCar(2);
-        me.garageValue();
-        me.garageSort();
-        auto1.sell(brother, me, 5000.0);
-        auto1.sell(brother, me, 2000.0);
-        auto2.sell(brother, me, 100.0);
-        auto1.sell(brother, me, 2000.0);
-        auto1.sell(brother, someone, 100.0);
-
-        auto1.lastOwner();
-        auto1.transactionList(someone, me);
-        auto1.transactionList(me, brother);
-        auto1.transactionCount();
-        auto1.wasOwner(someone);
-        auto1.wasOwner(me);
-
+        Application abirds = new Application("Angry Birds", "v. 2.3.9", 4.99);
+        Application spoti = new Application("Spotify", "v. 2.1.10", 0.0);
+        Application phmath = new Application("Photomath", "v. 1.4", 14.99);
+        Application wiri = new Application("Wild Rift", "v. 2.1", 0.0);
+        Application froggy = new Application("Żappka", "v. 1.6", 0.0);
+        Application mc = new Application("Minecraft", "1.16.2", 24.99);
+        Oneplus.installAnApp(abirds, me);
+        Oneplus.isAppInstalled(abirds);
+        Oneplus.isAppInstalled("Angry Birds");
+        Oneplus.isAppInstalled(spoti);
+        Oneplus.isAppInstalled("Spotify");
+        Oneplus.installAnApp(spoti, me);
+        Oneplus.installAnApp(phmath, me);
+        Oneplus.installedAppsValue();
+        Oneplus.installAnApp(wiri, me);
+        Oneplus.installAnApp(froggy, me);
+        Oneplus.freeApps();
+        Oneplus.installAnApp(mc, me);
+        Oneplus.installedAppsValue();
+        Oneplus.appsAlphabeticalSort();
+        Oneplus.appPriceSort();
     }
 }
--- devices/Phone.java
+++ devices/Phone.java
@@ -2,9 +2,8 @@ package com.company.devices;
 
 import com.company.Saleable;
 import com.company.creatures.Human;
-import java.net.URL;
-import java.net.MalformedURLException;
-import java.util.List;
+import java.net.*;
+import java.util.*;
 
 public class Phone extends Device implements Saleable {
     final Double screensize;
@@ -12,6 +11,7 @@ public class Phone extends Device implements Saleable {
     public static final String DEFAULT_appVersion = "1.0";
     public static final String DEFAULT_appServer = "play.google.com/store/apps";
     public static final String DEFAULT_protocol = "https";
+    public Set<Application> apps = new HashSet<Application>();
 
     public Phone(String model, String producer, Integer yearOfProduction, Double value, Double screensize, String os) {
         super(model, producer, yearOfProduction, value);
@@ -65,7 +65,76 @@ public class Phone extends Device implements Saleable {
             this.installAnApp(appName);
         }
     }
+
     public void installAnApp(URL urlAdress){
         System.out.println("Installing app from: " + urlAdress);
+    }
+
+    public void installAnApp(Application app, Human user){
+        if(user.cash >= app.price){
+            this.apps.add(app);
+            user.cash -= app.price;
+            System.out.println(app.name + " installed. Price: " + app.price + " zł.");
+        }
+        else{
+            System.out.println("Not enough money");
+        }
+    }
+
+    public boolean isAppInstalled(Application app){
+        if(this.apps.contains(app)){
+            System.out.println("Yes");
+            return true;
+        }
+        else{
+            System.out.println("No");
+            return false;
+        }
+    }
+
+    public boolean isAppInstalled(String appName){
+        Iterator<Application> app = apps.iterator();
+        while(app.hasNext()) {
+            if (app.next().name == appName) {
+                System.out.println("Yes");
+                return true;
+            }
+        }
+        System.out.println("No");
+        return false;
+    }
+
+    public void freeApps(){
+        for(Application app : apps){
+            if(app.price == 0.0){
+                System.out.println("Free app: " + app.name);
+            }
+        }
+    }
+
+    public void installedAppsValue(){
+        Double value = 0.0;
+        Iterator<Application> app = apps.iterator();
+        while(app.hasNext()){
+            value += app.next().price;
+        }
+        System.out.println("Money spent: " + value);
+    }
+
+    public void appsAlphabeticalSort(){
+        ArrayList<String> appNames = new ArrayList<String>();
+        for(Application app : apps){
+            appNames.add(app.name);
+        }
+        Collections.sort(appNames);
+        System.out.println(appNames);
+    }
+
+    public void appPriceSort(){
+        Application[] x = apps.toArray(new Application[apps.size()]);
+        Arrays.sort(x, Comparator.comparing(Application::getPrice));
+        for(int i = 0; i < x.length; i++){
+            System.out.println(x[i]);
+        }
     }
 }
